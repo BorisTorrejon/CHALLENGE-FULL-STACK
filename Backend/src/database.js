f6b92387b36ce770822243b2df9a47c4d4498ac0
Torrejon Boris <torrejonboris@gmail.com>
2021-11-26 12:17:50 -0300

    Backend Finished
    Fill in the CRUD, rename the database, and I gave  the index.html some styling

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host    :"localhost",
     user    :"root",
     password:"",
-    database:"transaction"
+    database:"personal_budget"
 });
 
 //Check connect
--- Backend/src/routes/transaction.router.js
+++ Backend/src/routes/transaction.router.js
@@ -1,10 +1,67 @@
 const express = require ('express');
+const connection = require('../database');
 const router  = express.Router();
 
 const Transaction = require ('../models/transaction');
 
 router.get('/', async(req, res)=>{
-    console.log('method get');
+    const sql = 'SELECT * FROM transactions';
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.json({status:"Not results"});
+    });
 });
+
+router.get('/:id', async(req, res)=>{
+    const {id}= req.params; 
+    const sql = `SELECT * FROM transactions WHERE transactionID=${id}`;
+    connection.query(sql,(error, result)=>{
+        if(error) throw error;
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.json({status:"Not result"});
+    });
+});
+
+router.post('/', async(req, res)=>{
+    const sql = 'INSERT INTO transactions SET ?';
+    const transaction = {
+        transactionConcept: req.body.concept,
+        transactionAmount : req.body.amount,
+        transactionDate   : req.body.date,
+        transactionType   : req.body.type
+    };
+    connection.query(sql,transaction,error=>{
+        if(error) throw error;
+        res.json({status:"your transaction was saved"});
+    });
+});
+
+router.put('/:id', async(req, res)=>{
+    const {id}= req.params;
+    const sql = `UPDATE transactions SET ? WHERE transactionID=${id}`;
+    const transaction = {
+        transactionConcept: req.body.concept,
+        transactionAmount : req.body.amount,
+        transactionDate   : req.body.date
+    }
+    connection.query(sql,transaction,error=>{
+        if(error) throw error;
+        res.json({status:"you transaction was updated"});
+    });
+});
+
+router.delete('/:id', async(req, res)=>{
+    const {id}=req.params;
+    const sql = `DELETE FROM transactions WHERE transactionID=${id}`;
+    connection.query(sql,error=>{
+        if(error) throw error;
+        res.json({status:"your transaction was deleted"});
+    });
+})
 
 module.exports = router;
